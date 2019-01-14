d0f74bb01b51d083c5a6b7af9db4c87d16b86b07
wallisyan01 <45842898+wallisyan01@users.noreply.github.com>
2019-01-14 16:38:10 +0800

    Create run_all_test.py

---
+++ python-sdk-functional-test/run_all_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from api_encapsulation_test import APIEncapsulateTest
+from bugs_test import BugsTest
+from core_test import CoreLevelTest
+from new_endpoint_test import NewEndpointTest
+from credentials_test import CredentialsTest
+from error_handle_test import ErrorHandleTest
+
+
+if __name__ == '__main__':
+    unittest.main()
