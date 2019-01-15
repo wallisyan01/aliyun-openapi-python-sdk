72dfca879a7c60c15df7b7d4052c46565de53c45
wallisyan01 <45842898+wallisyan01@users.noreply.github.com>
2019-01-15 13:30:14 +0800

    Update test_all.py

--- python-sdk-functional-test/test_all.py
+++ python-sdk-functional-test/test_all.py
@@ -8,9 +8,3 @@ for ret in os.walk(cur_path):
     if root_path_list[-1].startswith('aliyun-python-sdk-'):
         python_path += root_path + ';'
 os.environ.update({"PYTHONPATH":python_path})
-# path = os.environ.get('PYTHONPATH')
-# print(path)
-import subprocess
-cmd = 'coverage run -a --source="./aliyun-python-sdk-core/aliyunsdkcore" --branch -m pytest python-sdk-functional-test'
-ret = subprocess.check_call(cmd, shell=True)
-print(ret)
