00c31c7867fdac5588a46da0e22b15e6b3ca169d
wallisyan01 <45842898+wallisyan01@users.noreply.github.com>
2019-01-15 14:29:09 +0800

    Update test_all.py

--- python-sdk-functional-test/test_all.py
+++ python-sdk-functional-test/test_all.py
@@ -7,5 +7,6 @@ for ret in os.walk(cur_path):
     root_path = ret[0]
     root_path_list = root_path.split('\\')
     if root_path_list[-1].startswith('aliyun-python-sdk-'):
+        sys.path.insert(1,root_path)
         python_path += root_path + ';'
 os.environ.update({"PYTHONPATH":python_path})
