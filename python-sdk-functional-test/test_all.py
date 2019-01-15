44f74aedbb67f45a100f1a4f04c423b2e246775c
wallisyan01 <45842898+wallisyan01@users.noreply.github.com>
2019-01-15 12:37:53 +0800

    Update test_all.py

--- python-sdk-functional-test/test_all.py
+++ python-sdk-functional-test/test_all.py
@@ -10,3 +10,8 @@ for ret in os.walk(cur_path):
 os.environ.update({"PYTHONPATH":python_path})
 path = os.environ.get('PYTHONPATH')
 print(path)
+import subprocess
+cmd = 'coverage run -a --source="./aliyun-python-sdk-core/aliyunsdkcore" --branch -m pytest python-sdk-functional-test'
+ret = subprocess.check_call(cmd, shell=True)
+
+print(ret)
