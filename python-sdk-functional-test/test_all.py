ff22082c17ed2d1900b779e8cac4080a17d19120
wallisyan01 <45842898+wallisyan01@users.noreply.github.com>
2019-01-15 12:14:43 +0800

    Create test_all.py

---
+++ python-sdk-functional-test/test_all.py
@@ -0,0 +1,10 @@
+import os
+cur_path = os.path.abspath('..')
+python_path = ''
+for ret in os.walk(cur_path):
+    root_path = ret[0]
+    root_path_list = root_path.split('\\')
+    if root_path_list[-1].startswith('aliyun-python-sdk-'):
+        python_path += root_path + ';'
+os.environ.update({"PYTHONPATH":python_path})
+path = os.environ.get('PYTHONPATH')
